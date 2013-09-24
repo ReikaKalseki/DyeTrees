09231d70cffc59a170db3fbe0c02abd98718940a
Reika <reikasminecraft@gmail.com>
2013-09-24 15:48:23 -0400

    Update

--- API/TreeGetter.java
+++ API/TreeGetter.java
@@ -9,37 +9,53 @@
  ******************************************************************************/
 package Reika.DyeTrees.API;
 
+import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
-import Reika.DyeTrees.Registry.DyeBlocks;
 
 public class TreeGetter {
 
 	private static Class dyeTreeBlocks;
-	private static DyeBlocks[] dyeBlocks;
+	private static Block[] dyeBlocks;
 
 	public static boolean isDyeLeaf(ItemStack block) {
-		return block.itemID == dyeBlocks[0].LEAF.getBlockID();
+		return block.itemID == dyeBlocks[0].blockID;
 	}
 
 	public static boolean isDyeSapling(ItemStack block) {
-		return block.itemID == dyeBlocks[0].SAPLING.getBlockID();
+		return block.itemID == dyeBlocks[1].blockID;
 	}
 
 	public static ItemStack getDyeSapling(int dyeMeta) {
-		return new ItemStack(dyeBlocks[0].SAPLING.getBlockID(), 1, dyeMeta);
+		return new ItemStack(dyeBlocks[1].blockID, 1, dyeMeta);
 	}
 
 	public static ItemStack getDyeLeaf(int dyeMeta) {
-		return new ItemStack(dyeBlocks[0].LEAF.getBlockID(), 1, dyeMeta);
+		return new ItemStack(dyeBlocks[0].blockID, 1, dyeMeta);
 	}
 
 	static {
 		try {
-			dyeTreeBlocks = Class.forName("Reika.DyeTrees.Registry.DyeBlocks", false, TreeGetter.class.getClassLoader());
-			dyeBlocks = (DyeBlocks[])dyeTreeBlocks.getEnumConstants();
+			dyeTreeBlocks = Class.forName("Reika.DyeTrees.DyeTrees", false, TreeGetter.class.getClassLoader());
+			dyeBlocks = (Block[])dyeTreeBlocks.getField("blocks").get(null);
 		}
 		catch (ClassNotFoundException e) {
 			System.out.println("DyeTrees class not found!");
+			e.printStackTrace();
+		}
+		catch (IllegalArgumentException e) {
+			System.out.println("DyeTrees class not read correctly!");
+			e.printStackTrace();
+		}
+		catch (IllegalAccessException e) {
+			System.out.println("DyeTrees class not read correctly!");
+			e.printStackTrace();
+		}
+		catch (NoSuchFieldException e) {
+			System.out.println("DyeTrees class not read correctly!");
+			e.printStackTrace();
+		}
+		catch (SecurityException e) {
+			System.out.println("DyeTrees class not read correctly!");
 			e.printStackTrace();
 		}
 	}
