6b41666e5f2e34091929364ff50e9c30e2a8136c
Reika <reikasminecraft@gmail.com>
2014-02-21 02:51:28 -0500

    Update

--- World/RainbowForestGenerator.java
+++ World/RainbowForestGenerator.java
@@ -36,6 +36,10 @@ public class RainbowForestGenerator extends WorldGenerator {
 						for (int i = 0; i < 8; i++) {
 							int dx = ReikaRandomHelper.getRandomPlusMinus(x, 6);
 							int dz = ReikaRandomHelper.getRandomPlusMinus(x, 6);
+							while (!world.checkChunksExist(dx, 0, dz, dx, world.provider.getActualHeight(), dz)) {
+								dx = ReikaRandomHelper.getRandomPlusMinus(x, 6);
+								dz = ReikaRandomHelper.getRandomPlusMinus(x, 6);
+							}
 							int dy = world.getTopSolidOrLiquidBlock(dx, dz);
 							if (ReikaWorldHelper.softBlocks(world, dx, dy, dz)) {
 								int id = ThaumBlockHandler.getInstance().plantID;
