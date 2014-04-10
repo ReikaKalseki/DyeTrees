8074879d1842cdbe36c56e5f82baee8d757fff33
Reika <reikasminecraft@gmail.com>
2014-04-10 09:16:47 -0400

    Update

--- TileEntityRainbowBeacon.java
+++ TileEntityRainbowBeacon.java
@@ -71,7 +71,7 @@ public class TileEntityRainbowBeacon extends TileEntityBase {
 	}
 
 	@Override
-	public void animateWithTick(World world, int x, int y, int z) {
+	protected void animateWithTick(World world, int x, int y, int z) {
 
 	}
 
